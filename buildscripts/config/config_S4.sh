c6ea2020e11ed98562d347389d1aeedebd3af7c9
mmiesch <miesch@ucar.edu>
2020-08-28 09:49:11 -0600

    build flags not needed on S4 because we're now using a jedi-gcc module

--- buildscripts/config/config_S4.sh
+++ buildscripts/config/config_S4.sh
@@ -33,8 +33,8 @@ export WGET="wget -nv"
 #Global compiler flags
 export FFLAGS=""
 export CFLAGS=""
-export CXXFLAGS="-gxx-name=/opt/gcc/8.3/bin/g++ -std=c++14 -Wl,-rpath,/opt/gcc/8.3/lib64"
-export LDFLAGS="-gxx-name=/opt/gcc/8.3/bin/g++ -std=c++14 -Wl,-rpath,/opt/gcc/8.3/lib64"
+export CXXFLAGS="-std=c++14"
+export LDFLAGS="-std=c++14"
 
 # Minimal JEDI Stack
 export      STACK_BUILD_CMAKE=N
